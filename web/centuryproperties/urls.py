21153e449d4ebd22514561a9fa026da619a7e72b
Opio Daniel <danielopio540@gmail.com>
2024-03-13 18:42:08 +0200

    setup for production

--- web/centuryproperties/urls.py
+++ web/centuryproperties/urls.py
@@ -6,12 +6,16 @@ from django.conf import settings
 from django.views.static import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = ([
+urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('centuryproperties.apps.realestates.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 # if settings.DEBUG:
 #     urlpatterns = [
