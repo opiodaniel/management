8939543f6139bf825259e9b5e6f3a957f1a68378
Opio Daniel <danielopio540@gmail.com>
2024-03-15 15:53:06 +0200

    debug test

--- web/centuryproperties/settings/service.py
+++ web/centuryproperties/settings/service.py
@@ -25,9 +25,9 @@ DATABASES = {
 MEDIA_ROOT = '/home/opio/projects/management/web/centuryproperties/media/'  # for nginx
 STATIC_ROOT = '/home/opio/projects/management/web/static/'  # for nginx
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = 'AKIARK6LB2IT4PPO7SHH'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'acdantez'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
--- web/centuryproperties/urls.py
+++ web/centuryproperties/urls.py
@@ -13,13 +13,5 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
-# if settings.DEBUG:
-#     urlpatterns = [
-#         url(r'^media/(?P<path>.*)$', serve,
-#             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-#         ] + staticfiles_urlpatterns() + urlpatterns
 
